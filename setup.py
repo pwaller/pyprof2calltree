7f545344a8dc20e41a63e8c173da3b1028a6c12d
Jon Dufresne <jon.dufresne@gmail.com>
2018-10-07 12:28:17 -0700

    Add trove classifiers for modern Python versions

--- setup.py
+++ setup.py
@@ -8,11 +8,18 @@ Environment :: X11 Applications :: KDE
 License :: OSI Approved :: MIT License
 Operating System :: POSIX
 Operating System :: Unix
+Programming Language :: Python
+Programming Language :: Python :: 2
 Programming Language :: Python :: 2.5
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
 Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Topic :: Desktop Environment :: K Desktop Environment (KDE)
 Topic :: Software Development
 Topic :: Software Development :: Quality Assurance

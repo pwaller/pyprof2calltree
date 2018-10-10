pwaller/pyprof2calltree

commit 65e48a8226ebc4b2b634d169821935c99f9cef4d

Use context managers to close files in a deterministic manner
When pyprof2calltree is run with Python warnings enabled, fixes warnings
of the form:

  <file>:<line>: ResourceWarning: unclosed file <...>

pyprof2calltree.py
@@ -136,17 +136,14 @@ def pstats2entries(data):
 
 def is_installed(prog):
     """Return whether or not a given executable is installed on the machine."""
-    devnull = open(os.devnull, 'w')
-
-    try:
-        if os.name == 'nt':
-            retcode = subprocess.call(['where', prog], stdout=devnull)
-        else:
-            retcode = subprocess.call(['which', prog], stdout=devnull)
-    except FileNotFoundError:
-        retcode = 1
-
-    devnull.close()
+    with open(os.devnull, 'w') as devnull:
+        try:
+            if os.name == 'nt':
+                retcode = subprocess.call(['where', prog], stdout=devnull)
+            else:
+                retcode = subprocess.call(['which', prog], stdout=devnull)
+        except FileNotFoundError:
+            retcode = 1
 
     return retcode == 0
 
@@ -209,11 +206,12 @@ class CalltreeConverter(object):
         """
 
         if self.out_file is None:
-            _, outfile = tempfile.mkstemp(".log", "pyprof2calltree")
-            f = open(outfile, "w")
-            self.output(f)
+            fd, outfile = tempfile.mkstemp(".log", "pyprof2calltree")
+            with open(fd, "w") as f:
+                self.output(f)
             use_temp_file = True
         else:
+            outfile = self.out_file.name
             use_temp_file = False
 
         available_cmd = None
@@ -228,10 +226,8 @@ class CalltreeConverter(object):
                     ", ".join(KCACHEGRIND_EXECUTABLES))
             return
 
-        self.out_file.close()
-
         try:
-            subprocess.call([cmd, self.out_file.name])
+            subprocess.call([cmd, outfile])
         finally:
             # clean the temporary file
             if use_temp_file:
@@ -341,7 +337,8 @@ def main():
         # user either explicitly required output file or requested by not
         # explicitly asking to launch kcachegrind
         sys.stderr.write("writing converted data to: %s\n" % outfile)
-        kg.output(open(outfile, 'w'))
+        with open(outfile, 'w') as f:
+            kg.output(f)
 
     if options.kcachegrind:
         sys.stderr.write("launching kcachegrind\n")
@@ -373,11 +370,8 @@ def convert(profiling_data, outputfile):
     """
     converter = CalltreeConverter(profiling_data)
     if is_basestring(outputfile):
-        f = open(outputfile, "w")
-        try:
+        with open(outputfile, "w") as f:
             converter.output(f)
-        finally:
-            f.close()
     else:
         converter.output(outputfile)
 

setup.py
@@ -27,11 +27,17 @@ Topic :: System :: System Shells
 Topic :: Utilities
 """
 
+
+def readall(path):
+    with open(path) as f:
+        return f.read()
+
+
 setup(
     name='pyprof2calltree',
     version=version,
     description="Help visualize profiling data from cProfile with kcachegrind and qcachegrind",
-    long_description=open('README.rst').read(),
+    long_description=readall('README.rst'),
     keywords='profiler visualization programming tool kde kcachegrind qcachegrind',
     classifiers=[c for c in classifiers.split("\n") if c and c.strip()],
     author='Olivier Grisel',

commit 298ecb9a227f3886409552465d2591c27261d5b4

Fix Python 2 to open file descriptor using io.open()
On Python 2, open() does not support opening OS file descriptors. Use
io.open() instead which does. io.open() opens the file in text
mode (Unicode), so, use unicode_literals to always write Unicode strings
to files. This is more forward compatible with Python 3 behavior.

Fixes regression introduced in 65e48a8226ebc4b2b634d169821935c99f9cef4d.

pyprof2calltree.py
@@ -33,9 +33,12 @@ This script can either take raw cProfile.Profile.getstats() log entries or
 take a previously recorded instance of the pstats.Stats class.
 """
 
+from __future__ import unicode_literals
+
 import argparse
 import cProfile
 import errno
+import io
 import os
 import pstats
 import subprocess
@@ -234,7 +237,7 @@ class CalltreeConverter(object):
 
         try:
             if use_temp_file:
-                with open(fd, "w") as f:
+                with io.open(fd, "w") as f:
                     self.output(f)
             subprocess.call([available_cmd, outfile])
         finally:
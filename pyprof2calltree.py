pwaller/pyprof2calltree

commit f3dd542e3f50498798b7d15252adc019df67ba9e

Drop official support for EOL Pythons 2.5, 2.6, 3.2, and 3.3
Python 2.5, 2.6, 3.2, and 3.3 are end of life. They are no longer
receiving bug fixes, including for security issues. Python 2.6 went EOL
on 2013-10-29 and Python 3.3 on 2017-09-29. For additional details on
support Python versions, see:

https://devguide.python.org/devcycle/#end-of-life-branches

Removing support for EOL Pythons will reduce testing and maintenance
resources. The test matrix now matches the support matrix.

Full list of changes:

- Remove trove classifiers for older Python
- Replace deprecated optparse with argparse
- Pass python_requires argument to setuptools

Using pypinfo, here are the PyPI download statistics of the projects for
the last 30 days. Showing extremely little use from EOL Pythons.

$ pypinfo --percent --markdown pyprof2calltree pyversion

| python_version | percent | download_count |
| -------------- | ------: | -------------: |
| 2.7            |  54.50% |          4,347 |
| 3.6            |  31.53% |          2,515 |
| 3.5            |   8.84% |            705 |
| 3.7            |   3.44% |            274 |
| 3.4            |   1.59% |            127 |
| 2.6            |   0.10% |              8 |
| Total          |         |          7,976 |

pyprof2calltree.py
@@ -33,9 +33,9 @@ This script can either take raw cProfile.Profile.getstats() log entries or
 take a previously recorded instance of the pstats.Stats class.
 """
 
+import argparse
 import cProfile
 import errno
-import optparse
 import os
 import pstats
 import subprocess
@@ -286,30 +286,27 @@ class CalltreeConverter(object):
 def main():
     """Execute the converter using parameters provided on the command line"""
 
-    usage = ("%s [-k] [-o output_file_path] [-i input_file_path]"
-             " [-r scriptfile [args]]")
-    parser = optparse.OptionParser(usage=usage % sys.argv[0])
-    parser.allow_interspersed_args = False
-    parser.add_option('-o', '--outfile', dest="outfile",
-                      help="Save calltree stats to <outfile>", default=None)
-    parser.add_option('-i', '--infile', dest="infile",
-                      help="Read Python stats from <infile>", default=None)
-    parser.add_option('-r', '--run-script', dest="script",
-                      help="Name of the Python script to run to collect"
-                      " profiling data", default=None)
-    parser.add_option('-k', '--kcachegrind', dest="kcachegrind",
-                      help="Run the kcachegrind tool on the converted data",
-                      action="store_true")
-    options, args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--outfile',
+                        help="Save calltree stats to <outfile>")
+    parser.add_argument('-i', '--infile',
+                        help="Read Python stats from <infile>")
+    parser.add_argument('-k', '--kcachegrind',
+                        help="Run the kcachegrind tool on the converted data",
+                        action="store_true")
+    parser.add_argument('-r', '--run-script', nargs='+', dest='script',
+                        help="Name of the Python script to run to collect"
+                        " profiling data")
+    args = parser.parse_args()
 
-    outfile = options.outfile
+    outfile = args.outfile
 
-    if options.script is not None:
+    if args.script is not None:
         # collect profiling data by running the given script
 
-        sys.argv[:] = [options.script] + args
-        if not options.outfile:
-            outfile = '%s.log' % os.path.basename(options.script)
+        sys.argv[:] = args.script
+        if not args.outfile:
+            outfile = '%s.log' % os.path.basename(args.script[0])
 
         prof = cProfile.Profile()
 
@@ -326,30 +323,30 @@ def main():
         finally:
             kg = CalltreeConverter(pstats.Stats(prof))
 
-    elif options.infile is not None:
+    elif args.infile is not None:
         # use the profiling data from some input file
-        if not options.outfile:
-            outfile = '%s.log' % os.path.basename(options.infile)
+        if not args.outfile:
+            outfile = '%s.log' % os.path.basename(args.infile)
 
-        if options.infile == outfile:
+        if args.infile == outfile:
             # prevent name collisions by appending another extension
             outfile += ".log"
 
-        kg = CalltreeConverter(pstats.Stats(options.infile))
+        kg = CalltreeConverter(pstats.Stats(args.infile))
 
     else:
         # at least an input file or a script to run is required
         parser.print_usage()
         sys.exit(2)
 
-    if options.outfile is not None or not options.kcachegrind:
+    if args.outfile is not None or not args.kcachegrind:
         # user either explicitly required output file or requested by not
         # explicitly asking to launch kcachegrind
         sys.stderr.write("writing converted data to: %s\n" % outfile)
         with open(outfile, 'w') as f:
             kg.output(f)
 
-    if options.kcachegrind:
+    if args.kcachegrind:
         sys.stderr.write("launching kcachegrind\n")
         kg.visualize()
 

setup.py
@@ -25,12 +25,8 @@ setup(
         "Operating System :: Unix",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
@@ -48,6 +44,7 @@ setup(
     url='https://github.com/pwaller/pyprof2calltree/',
     license='MIT',
     py_modules=['pyprof2calltree'],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     zip_safe=True,
     test_suite='test',
     entry_points={

commit 49de7fed313cb59d70a574d7cd53a2886836be9c

Use REMAINDER for --run-script to allow passing arbitrary arguments
Regression mistakenly introduced in
f3dd542e3f50498798b7d15252adc019df67ba9e.

pyprof2calltree.py
@@ -298,7 +298,9 @@ def main():
                         help="Run the kcachegrind tool on the converted data",
                         action="store_true")
     parser.add_argument('-r', '--run-script',
-                        nargs='+', metavar=('scriptfile', 'args'), dest='script',
+                        nargs=argparse.REMAINDER,
+                        metavar=('scriptfile', 'args'),
+                        dest='script',
                         help="Name of the Python script to run to collect"
                         " profiling data")
     args = parser.parse_args()
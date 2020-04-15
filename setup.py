811ff11017f2aca5fa8b33bd527bc562e5d4c02f
Jon Dufresne <jon.dufresne@gmail.com>
2020-04-14 18:38:23 -0700

    Add testing and support for Python 3.8
    Python 3.8 was released on October 14th, 2019.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Desktop Environment :: K Desktop Environment (KDE)",
         "Topic :: Software Development",
         "Topic :: Software Development :: Quality Assurance",
